c054a93b073892a6067cc51651dcb726f971bb32
Jung Palbong <p_jung@where123.jp>
2018-07-11 11:56:57 +0900

    TX表示種別DB名変更による対応

--- app/models/ViewType.scala
+++ app/models/ViewType.scala
@@ -8,7 +8,10 @@ import play.api.db._
 
 case class ViewType(
   view_type_id: Int,
-  view_type_name: String
+  view_type_name: String,
+  note  :String,
+  place_id :Int,
+  active_flg :Boolean
 )
 
 case class ViewTypeData(
@@ -24,18 +27,36 @@ class ViewTypeDAO @Inject() (dbapi: DBApi) {
   // Parser
   val simple = {
     get[Int]("view_type_id") ~
-      get[String]("view_type_name") map {
-        case view_type_id ~ view_type_name =>
-          ViewType(view_type_id, view_type_name)
+      get[String]("view_type_name")~
+      get[String]("note") ~
+      get[Int]("place_id") ~
+      get[Boolean]("active_flg") map {
+        case view_type_id ~ view_type_name  ~ note ~ place_id ~ active_flg=>
+          ViewType(view_type_id, view_type_name,note ,place_id ,active_flg)
       }
   }
 
   def selectAll(): Seq[ViewType] = {
     db.withConnection { implicit connection =>
       val sql = SQL("""
-              select view_type_id, view_type_name
-              from view_type_master order by view_type_id;
+              select view_type_id, view_type_name ,note ,place_id ,active_flg
+              from view_type order by view_type_id;
               """)
+      sql.as(simple.*)
+    }
+  }
+
+  def selectViewInfo(placeId: Int): Seq[ViewType] = {
+    db.withConnection { implicit connection =>
+      val sql = SQL("""
+              select view_type_id ,view_type_name ,note ,place_id ,active_flg
+              from view_type
+              where place_id = {placeId}
+              and active_flg = true
+              order by view_type_id;
+              """).on(
+        "placeId" -> placeId
+      )
       sql.as(simple.*)
     }
   }
