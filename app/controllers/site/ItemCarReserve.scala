38f89cb53e09605b56ff33528026a1a56d70e028
Jung Palbong <p_jung@where123.jp>
2018-07-18 14:57:30 +0900

    作業車・立馬予約画面追加（中身修正必要）

---
+++ app/controllers/site/ItemCarReserve.scala
@@ -0,0 +1,121 @@
+package controllers.site
+
+import javax.inject.{Inject, Singleton}
+
+import com.mohiva.play.silhouette.api.Silhouette
+import controllers.{BaseController, BeaconService}
+import models._
+import play.api._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import utils.silhouette.MyEnv
+
+
+/**
+  * 作業車・立馬予約画面
+  *
+  *
+  */
+
+
+@Singleton
+class ItemCarReserve @Inject()(config: Configuration
+                               , val silhouette: Silhouette[MyEnv]
+                               , val messagesApi: MessagesApi
+                               , carDAO: models.itemCarDAO
+                               , companyDAO: models.companyDAO
+                               , beaconService: BeaconService
+                               , floorDAO: models.floorDAO
+                               , btxDAO: models.btxDAO
+                               , itemTypeDAO: models.ItemTypeDAO
+                               , workTypeDAO: models.WorkTypeDAO
+                             ) extends BaseController with I18nSupport {
+
+  var ITEM_TYPE_FILTER = 0;
+  var FLOOR_NAME_FILTER = "";
+  var WORK_TYPE_FILTER = "";
+
+  var itemTypeList :Seq[ItemType] = null; // 仮設材種別
+  var companyNameList :Seq[Company] = null; // 業者
+  var floorNameList :Seq[Floor] = null; // フロア
+  var workTypeList :Seq[WorkType] = null; // 作業期間種別
+
+  /*enum形*/
+  val WORK_TYPE = WorkTypeEnum().map;
+
+  /*転送form*/
+  val carForm = Form(mapping(
+    "itemTypeId" -> number,
+    "companyName" -> text,
+    "floorName" -> text,
+    "workTypeName" -> text
+  )(ItemCarData.apply)(ItemCarData.unapply))
+
+  /** 　初期化 */
+  def init() {
+    ITEM_TYPE_FILTER = 0
+    WORK_TYPE_FILTER = ""
+  }
+
+  /** 　検索側データ取得 */
+  def getSearchData(_placeId:Integer): Unit = {
+    /*仮設材種別取得*/
+    itemTypeList = itemTypeDAO.selectItemCarInfo(_placeId);
+    /*業者取得*/
+    companyNameList = companyDAO.selectCompany(_placeId);
+    /*フロア取得*/
+    floorNameList = floorDAO.selectFloor(_placeId);
+    /*作業期間種別取得*/
+    workTypeList = workTypeDAO.selectWorkInfo(_placeId);
+  }
+
+  /** 　検索ロジック */
+  def search = SecuredAction { implicit request =>
+    System.out.println("start search:")
+    val placeId = super.getCurrentPlaceId
+    //検索側データ取得
+    getSearchData(placeId)
+
+    // 部署情報
+    val carFormData = carForm.bindFromRequest.get
+    ITEM_TYPE_FILTER = carFormData.itemTypeId
+    WORK_TYPE_FILTER = carFormData.workTypeName
+
+    var carListApi = beaconService.getItemCarBeaconPosition(true,placeId)
+
+    if (ITEM_TYPE_FILTER != 0) {
+      carListApi = carListApi.filter(_.item_type_id == ITEM_TYPE_FILTER)
+    }
+    if (WORK_TYPE_FILTER != "") {
+      carListApi = carListApi.filter(_.work_type_name == WORK_TYPE_FILTER)
+    }
+
+    Ok(views.html.site.itemCarReserve(ITEM_TYPE_FILTER,WORK_TYPE_FILTER
+      ,carListApi,itemTypeList,companyNameList,floorNameList,workTypeList,WORK_TYPE))
+  }
+
+  /** 初期表示 */
+  def index = SecuredAction { implicit request =>
+    if (super.isCmsLogged) {
+      // 初期化
+      init();
+      val placeId = super.getCurrentPlaceId
+      //検索側データ取得
+      getSearchData(placeId)
+
+      // dbデータ取得
+      var carListApi = beaconService.getItemCarBeaconPosition(true,placeId)
+
+      // 全体から空いてるものだけ表示する。
+      carListApi = carListApi.filter(_.reserve_id == -1)
+
+      System.out.println("carListApi:" + carListApi.length)
+      Ok(views.html.site.itemCarReserve(ITEM_TYPE_FILTER,WORK_TYPE_FILTER
+        ,carListApi,itemTypeList,companyNameList,floorNameList,workTypeList,WORK_TYPE))
+    } else {
+      Redirect(CMS_NOT_LOGGED_RETURN_PATH).flashing(ERROR_MSG_KEY -> Messages("error.cmsLogged.invalid"))
+    }
+  }
+
+}
---
+++ public/javascripts/site/itemCarReserve.js
@@ -0,0 +1,157 @@
+// マウス・タッチの動作のバインド
+function bindMouseAndTouch(){
+    var ua = navigator.userAgent;
+    if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0 || ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0){
+        // タッチデバイスの場合
+        $(".rowHover").bind({
+            'touchstart': function(e) {
+                if(e.originalEvent.touches.length > 1){
+                }else if(e.originalEvent.touches.length == 1){
+                    $(".rowHoverSelectedColor").removeClass('rowHoverSelectedColor');
+                    $(this).addClass('rowHoverSelectedColor');
+                }
+            },
+        });
+    }else{
+        // PCブラウザの場合
+        $(".rowHover").bind({
+            'mouseover': function(e) {
+                $(this).addClass('rowHoverColor');
+            },
+            'mouseout': function(e) {
+                $(this).removeClass('rowHoverColor');
+            },
+            'click': function(e) {
+                $(".rowHoverSelectedColor").removeClass('rowHoverSelectedColor');
+                $(this).addClass('rowHoverSelectedColor');
+            },
+        });
+    }
+}
+
+// テーブルの固定
+function fixTable(){
+    // テーブルの固定
+    var h = $(window).height()*0.8;
+    // テーブルの調整
+    var ua = navigator.userAgent;
+    if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0 || ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0){
+        // タッチデバイス
+        if ($('.mainSpace').height() > h) {
+            var w = $('.mainSpace').width()*0.99;
+            $('.itemTable').tablefix({width:w, height: h, fixRows: 2});
+        } else {
+            var w = $('.mainSpace').width();
+            $('.itemTable').tablefix({width:w, fixRows: 2});
+        }
+    }else{
+//        // PCブラウザ
+//        var w = $('.mainSpace').width()*1.001; // ヘッダー右側ボーダーが切れる為*1.001
+//        if ($('.mainSpace').height() > h) {
+//            w = $('.mainSpace').width()-5;
+//            $('.itemTable').tablefix({width:w, height: h, fixRows: 2});
+//            w = $('.mainSpace').width()-14;
+//            $('.rowTableDiv').width(w);
+//        } else {
+//            $('.rowTableDiv').width(w);
+//        }
+        // PCブラウザ
+        var w = $('.mainSpace').width();
+        $('.itemTable').tablefix({height: h, fixRows: 2});
+        $('.rowTableDiv').width(w);
+    }
+    $('.bodyTableDiv').find('.itemTable').css('margin-bottom','0');
+    $('.colTableDiv').css("width","");
+
+}
+// テーブルのクリア
+function removeTable(){
+    var clonedTable = $('.bodyTableDiv').find('table').clone();
+    $(clonedTable).attr('style', '');
+    $('.baseDiv').remove();
+    $('.table-responsive-body').append(clonedTable.prop("outerHTML"));
+}
+
+// サブミット
+function doSubmit(formId, action){
+    $('#' + formId).attr('action', action)
+    $('#' + formId).submit();
+}
+
+
+// サブミット
+function getFilterCheck(){
+    var inputItemType = document.getElementById("itemTypeId")
+    if(inputItemType!=null){
+         $('#ITEM_TYPE_FILTER').val(inputItemType.value)
+    }
+    var inputCompanyName = document.getElementById("companyName")
+    if(inputCompanyName!=null){
+         $('#COMPANY_NAME_FILTER').val(inputCompanyName.value)
+    }
+    var inputFloorName = document.getElementById("floorName")
+    if(inputFloorName!=null){
+         $('#FLOOR_NAME_FILTER').val(inputFloorName.value)
+    }
+    var inputWorkTypeName = document.getElementById("workTypeName")
+    if(inputWorkTypeName!=null){
+         $('#WORK_TYPE_FILTER').val(inputWorkTypeName.value)
+    }
+}
+// 表示ボタンをクリックする時に発生するイベント
+function viewBtnEvent(){
+
+    // DatePickerの設定 start-----------------------------------------
+     gDatePicker.dayClickEvent();
+     gDatePicker.htmlDayClickEvent();
+    // DatePickerの設定 end-----------------------------------------
+
+    var viewBtnElement = document.getElementsByClassName("btn__view--frame")[0];
+    viewBtnElement.addEventListener('click', function(event) {
+        // itemTypeId結果をfromへ設定
+        var itemTypeFilterResult = $('#ITEM_TYPE_FILTER option:selected').val();
+        var inputItemType = document.getElementById("itemTypeId")
+        inputItemType.value = itemTypeFilterResult
+
+        // work_type_name結果をfromへ設定
+        var workTypeNameFilterResult = $('#WORK_TYPE_FILTER option:selected').val();
+        var inputWorkTypeName = document.getElementById("workTypeName")
+        inputWorkTypeName.value = workTypeNameFilterResult
+
+        var formElement = $("#viewForm")
+        formElement[0].action = "../site/itemCarMaster"
+        // 送信ボタン生成
+        var vButton = document.createElement("button");
+        vButton.id = "dbExecuteBtn"
+        vButton.className = "btn hidden";
+        formElement[0].appendChild(vButton);
+
+        $("#dbExecuteBtn").trigger( "click" );
+    });
+
+}
+$(function(){
+    // filter値確認
+    getFilterCheck();
+    // 表示ボタンをクリック
+    viewBtnEvent();
+
+    // テーブルを固定
+    fixTable();
+    // マウス操作とタップ操作をバインド
+    bindMouseAndTouch();
+
+    // リサイズ対応
+    var timer = false;
+    $(window).resize(function() {
+        if (timer !== false) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function() {
+            // 処理の再実行
+            removeTable();
+            fixTable();
+            bindMouseAndTouch();
+        }, 200);
+    });
+});
