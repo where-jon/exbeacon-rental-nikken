334a0e99ba37b5dbaf8600f02e8dd5ac76df16ee
Jung Palbong <p_jung@where123.jp>
2018-08-17 20:56:08 +0900

    issue/ 83 分析月未入力エラー表示メッセージ表示できないもの解決

--- app/controllers/analysis/MovementCar.scala
+++ app/controllers/analysis/MovementCar.scala
@@ -351,7 +351,8 @@ class MovementCar @Inject()(config: Configuration
     movementCarSearchForm.bindFromRequest.fold(
       formWithErrors =>
         Redirect(routes.MovementCar.index())
-          .flashing(ERROR_MSG_KEY -> Messages(formWithErrors.errors.map(_.message +"<br>").mkString("\n"))),
+          //.flashing(ERROR_MSG_KEY -> Messages(formWithErrors.errors.map(_.message +"<br>").mkString("\n"))),  //herokuで動かない
+            .flashing(ERROR_MSG_KEY -> Messages("error.analysis.movementCar.search.date.empty")),
       searchForm => {
         val placeId = super.getCurrentPlaceId
         //検索側データ取得
