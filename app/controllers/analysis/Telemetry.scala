8003b023ae62b9657a49457694270038dcea06ac
Jung Palbong <p_jung@where123.jp>
2018-08-23 20:07:55 +0900

    exb状態監視画面作成中（初期表示のみ完了）

--- app/controllers/analysis/Telemetry.scala
+++ app/controllers/analysis/Telemetry.scala
@@ -4,7 +4,6 @@ import javax.inject.{Inject, Singleton}
 
 import com.mohiva.play.silhouette.api.Silhouette
 import controllers.{BaseController, BeaconService, site}
-import org.joda.time.DateTime
 import play.api._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.Json
@@ -12,12 +11,13 @@ import play.api.libs.ws._
 import utils.silhouette.MyEnv
 
 /**
-  * GW状態監視
+  * EXB状態監視
   *
   *
   */
 @Singleton
-class Telemetry @Inject()(config: Configuration
+class Telemetry @Inject()(
+   config: Configuration
   , val silhouette: Silhouette[MyEnv]
   , val messagesApi: MessagesApi
   , beaconService: BeaconService
@@ -27,23 +27,27 @@ class Telemetry @Inject()(config: Configuration
   ) extends BaseController with I18nSupport {
 
 
-
+  var placeId : Int = -1
   /* 仮設材テーブルと予約テーブルとapiを結合したデータを取得*/
   def getData = SecuredAction{ implicit request =>
     val placeId = super.getCurrentPlaceId
-    val gatewayListApi = beaconService.getGateWayState(placeId)
+    val gatewayListApi = beaconService.getTelemetryState(placeId)
     Ok(Json.toJson(gatewayListApi))
-  }
 
+  }
 
     /**
     * 初期表示
     * @return
     */
   def index = SecuredAction { implicit request =>
+     placeId = super.getCurrentPlaceId
     val reqIdentity = request.identity
     if(reqIdentity.level >= 2){
-      Ok(views.html.analysis.telemetry())
+      val placeId = super.getCurrentPlaceId
+      val exbListApi = beaconService.getTelemetryState(placeId)
+      Ok(views.html.analysis.telemetry(exbListApi))
+
     }else{
       Redirect(site.routes.WorkPlace.index)
     }
