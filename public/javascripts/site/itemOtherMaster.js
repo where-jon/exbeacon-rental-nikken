81ace6164be48446210606d5196ae494ec1592c8
Jung Palbong <p_jung@where123.jp>
2018-08-29 12:34:52 +0900

    既存から共通関数で画面resizeする

--- public/javascripts/site/itemOtherMaster.js
+++ public/javascripts/site/itemOtherMaster.js
@@ -1,77 +1,3 @@
-// マウス・タッチの動作のバインド
-function bindMouseAndTouch(){
-    var ua = navigator.userAgent;
-    if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0 || ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0){
-        // タッチデバイスの場合
-        $(".rowHover").bind({
-            'touchstart': function(e) {
-                if(e.originalEvent.touches.length > 1){
-                }else if(e.originalEvent.touches.length == 1){
-                    $(".rowHoverSelectedColor").removeClass('rowHoverSelectedColor');
-                    $(this).addClass('rowHoverSelectedColor');
-                }
-            },
-        });
-    }else{
-        // PCブラウザの場合
-        $(".rowHover").bind({
-            'mouseover': function(e) {
-                $(this).addClass('rowHoverColor');
-            },
-            'mouseout': function(e) {
-                $(this).removeClass('rowHoverColor');
-            },
-            'click': function(e) {
-                $(".rowHoverSelectedColor").removeClass('rowHoverSelectedColor');
-                $(this).addClass('rowHoverSelectedColor');
-            },
-        });
-    }
-}
-
-// テーブルの固定
-function fixTable(){
-    // テーブルの固定
-    var h = $(window).height()*0.8;
-    // テーブルの調整
-    var ua = navigator.userAgent;
-    if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0 || ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0){
-        // タッチデバイス
-        if ($('.mainSpace').height() > h) {
-            var w = $('.mainSpace').width()*0.99;
-            $('.itemTable').tablefix({width:w, height: h, fixRows: 2});
-        } else {
-            var w = $('.mainSpace').width();
-            $('.itemTable').tablefix({width:w, fixRows: 2});
-        }
-    }else{
-//        // PCブラウザ
-//        var w = $('.mainSpace').width()*1.001; // ヘッダー右側ボーダーが切れる為*1.001
-//        if ($('.mainSpace').height() > h) {
-//            w = $('.mainSpace').width()-5;
-//            $('.itemTable').tablefix({width:w, height: h, fixRows: 2});
-//            w = $('.mainSpace').width()-14;
-//            $('.rowTableDiv').width(w);
-//        } else {
-//            $('.rowTableDiv').width(w);
-//        }
-        // PCブラウザ
-        var w = $('.mainSpace').width();
-        $('.itemTable').tablefix({height: h, fixRows: 2});
-        $('.rowTableDiv').width(w);
-    }
-    $('.bodyTableDiv').find('.itemTable').css('margin-bottom','0');
-    $('.colTableDiv').css("width","");
-
-}
-// テーブルのクリア
-function removeTable(){
-    var clonedTable = $('.bodyTableDiv').find('table').clone();
-    $(clonedTable).attr('style', '');
-    $('.baseDiv').remove();
-    $('.table-responsive-body').append(clonedTable.prop("outerHTML"));
-}
-
 // サブミット
 function doSubmit(formId, action){
     $('#' + formId).attr('action', action)
@@ -140,21 +66,11 @@ $(function(){
     viewBtnEvent();
 
     // テーブルを固定
-    fixTable();
-    // マウス操作とタップ操作をバインド
-    bindMouseAndTouch();
+    gInitView.fixTableNoBtn();
 
-    // リサイズ対応
-    var timer = false;
-    $(window).resize(function() {
-        if (timer !== false) {
-            clearTimeout(timer);
-        }
-        timer = setTimeout(function() {
-            // 処理の再実行
-            removeTable();
-            fixTable();
-            bindMouseAndTouch();
-        }, 200);
-    });
+    // マウス操作とタップ操作をバインド
+    gInitView.bindMouseAndTouch();
+
+    // 画面サイズ変更による再調整
+    gInitView.tableResize("NoBtn");
 });
