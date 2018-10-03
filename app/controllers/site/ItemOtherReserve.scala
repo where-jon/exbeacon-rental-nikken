63bfc0d71e83be4aaadbf632edd1668e390e53b2
Jung Palbong <p_jung@where123.jp>
2018-10-03 11:59:34 +0900

    その他仮設材予約画面（予約・取消最大値検知）

--- app/controllers/site/ItemOtherReserve.scala
+++ app/controllers/site/ItemOtherReserve.scala
@@ -57,6 +57,8 @@ class ItemOtherReserve @Inject()(config: Configuration
   /*enum形*/
   val WORK_TYPE = WorkTypeEnum().map;
 
+  var RESERVE_MAX_COUNT = -1
+
 
     val itemOtherSearchForm = Form(mapping(
     "itemTypeId" ->number,
@@ -74,6 +76,8 @@ class ItemOtherReserve @Inject()(config: Configuration
 
     COMPANY_NAME_FILTER = ""
     FLOOR_NAME_FILTER = ""
+
+    RESERVE_MAX_COUNT = config.getInt("web.positioning.reserveMaxCount").get
   }
 
   /** 　検索側データ取得 */
@@ -222,7 +226,7 @@ class ItemOtherReserve @Inject()(config: Configuration
     }
 
     Ok(views.html.site.itemOtherReserve(ITEM_TYPE_FILTER,WORK_TYPE_FILTER,RESERVE_START_DATE,RESERVE_END_DATE
-      ,otherListApi,itemTypeList,companyNameList,floorNameList,workTypeList,WORK_TYPE))
+      ,otherListApi,itemTypeList,companyNameList,floorNameList,workTypeList,WORK_TYPE,RESERVE_MAX_COUNT))
   }
 
   /** 初期表示 */
@@ -239,7 +243,7 @@ class ItemOtherReserve @Inject()(config: Configuration
       val otherListApi = beaconService.getItemOtherBeaconPosition(dbDatas,true,placeId)
       if(otherListApi!=null){
         Ok(views.html.site.itemOtherReserve(ITEM_TYPE_FILTER,WORK_TYPE_FILTER,RESERVE_START_DATE,RESERVE_END_DATE
-          ,otherListApi,itemTypeList,companyNameList,floorNameList,workTypeList,WORK_TYPE))
+          ,otherListApi,itemTypeList,companyNameList,floorNameList,workTypeList,WORK_TYPE,RESERVE_MAX_COUNT))
       }else{
         // apiと登録データが違う場合
         Redirect(errors.routes.UnDetectedApi.indexSite)
