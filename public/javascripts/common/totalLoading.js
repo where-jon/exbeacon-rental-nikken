0299393c75944454846b0ea23c13f1db7b4d1d53
Jung Palbong <p_jung@where123.jp>
2018-10-04 18:36:13 +0900

    選択メニューボタン活性化の為修正

--- public/javascripts/common/totalLoading.js
+++ public/javascripts/common/totalLoading.js
@@ -1,6 +1,35 @@
-
+// 現在画面タイトル名を取得する
+function getSerfaceText(element){
+    var text = "";
+    for (var i = 0; i < element.childNodes.length; i++) {
+        if (element.childNodes[i].toString() == '[object Text]'){
+            text +=
+                element.childNodes[i].data;
+        }
+    }
+    return text.trim();
+}
+// 画面loadingが終わってから走る
 $(window).load(function() {
     $('#load').hide();
     // bodyにあるスクロールバーを削除
     document.body.style.overflow = "hidden"
+
+    // 左サイドメニュークリックしたら活性化させる .start
+    var vElement = $(".mainSpace").find('.form-group')
+    var vTitle = getSerfaceText(vElement[0])
+    var selectedElement = [].slice.call(document.querySelectorAll(".menuBtn"))
+    selectedElement.forEach(function(selected, pos) {
+        selected.addEventListener('click', function() {
+            selectedElement.forEach(function(selected, pos) {
+                 $(selected).removeClass('btnSelected');
+            });
+        });
+    });
+
+    selectedElement.forEach(function(selected, pos) {
+    if(selected.textContent.trim() == vTitle)
+         $(selected).addClass('btnSelected');
+    });
+    // 左サイドメニュークリックしたら活性化させる .end
 });
