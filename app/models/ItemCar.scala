8ba979f83d24d5a5e42a8520076efb0a3463672c
Jung Palbong <p_jung@where123.jp>
2018-10-12 12:38:02 +0900

    一覧画面に重複チェックロジックを入れる

--- app/models/ItemCar.scala
+++ app/models/ItemCar.scala
@@ -112,8 +112,8 @@ case class CarViewer(
   , reserve_start_date:String
   , company_id: Int
   , company_name: String
-  , work_type_id: Int
-  , work_type_name: String
+  , var work_type_id: Int
+  , var work_type_name: String
   , reserve_floor_name: String
   , reserve_id: Int
 )
@@ -498,6 +498,52 @@ class itemCarDAO @Inject()(dbapi: DBApi) {
           work_type_name,reserve_floor_name,reserve_id)
     }
   }
+
+  /*作業車・立馬一覧用 sql文 20180718*/
+  def selectCarMasterSql(placeId: Int,itemIdList:Seq[Int]): Seq[CarViewer] = {
+    db.withConnection { implicit connection =>
+      val selectPh =
+        """
+          select
+                c.item_car_id
+               ,c.item_car_btx_id
+               , c.item_car_key_btx_id
+               , c.item_type_id
+               , i.item_type_name
+               , c.note
+               , c.item_car_no
+               , c.item_car_name
+               , c.place_id
+               ,coalesce(to_char(r.reserve_start_date, 'YYYY-MM-DD'), 'date') as reserve_start_date
+               ,coalesce(r.company_id, -1) as company_id
+               ,coalesce(co.company_name, '無') as company_name
+               ,coalesce(work.work_type_id, -1) as work_type_id
+               ,coalesce(work.work_type_name, '無') as work_type_name
+               ,coalesce(floor.floor_name, '無') as reserve_floor_name
+               ,coalesce(r.reserve_id, -1) as reserve_id
+           from
+             		item_car_master as c
+             		LEFT JOIN reserve_table as r on c.item_car_id = r.item_id
+                and to_char(r.reserve_start_date, 'YYYY-MM-DD') = to_char(current_timestamp, 'YYYY-MM-DD')
+             		and r.active_flg = true
+						left JOIN item_type as i on i.item_type_id = c.item_type_id
+	             		and i.active_flg = true
+		             		left JOIN company_master as co on co.company_id = r.company_id
+		             		and co.active_flg = true
+			             		left JOIN work_type as work on work.work_type_id = r.work_type_id
+			             		and work.active_flg = true
+			             			left JOIN floor_master as floor on floor.floor_id = r.floor_id
+				             		and floor.active_flg = true
+           where c.place_id = """  + {placeId} + """
+           and c.active_flg = true
+           and c.item_type_id in ( """ + {itemIdList.mkString(",")} +""" )
+           order by item_car_btx_id ;
+
+        """
+      SQL(selectPh).as(carMasterViewer.*)
+    }
+  }
+
 
   /*作業車・立馬一覧用 sql文 20180718*/
   def selectCarMasterViewer(placeId: Int,itemIdList:Seq[Int]): Seq[CarViewer] = {
