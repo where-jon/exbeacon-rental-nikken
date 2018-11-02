e284cdbab4551fce0191cf05fe33e16f805c350f
t-kitahara <t_kitahara@where123.jp>
2018-11-02 14:53:33 +0900

    ・メール送信、送信曜日調整対応 mapをforeachに修正

--- app/actors/NoticeMailActor.scala
+++ app/actors/NoticeMailActor.scala
@@ -110,7 +110,7 @@ class NoticeMailActor @Inject()(config: Configuration
           val targetMonth = ymdf.format(cal.getTime())
 
           val placeData = placeDAO.selectPlaceAll(WORKING_STATUS)
-          placeData.zipWithIndex.map { case (place, i) =>
+          placeData.zipWithIndex.foreach { case (place, i) =>
             // ログ削除除外現場チェック
             val exclusionId = exclusionPlaceID.find(_ == place.placeId.toString)
             if (place.btxApiUrl != null && place.btxApiUrl != "" && exclusionId.isEmpty) {
