44672009dee1ce3ebc4be04629cef08e41f856f9
t-kitahara <t_kitahara@where123.jp>
2018-11-05 17:42:34 +0900

    ・メール送信、送信曜日調整対応

--- app/models/NoticeMail.scala
+++ app/models/NoticeMail.scala
@@ -9,10 +9,15 @@ import play.libs.mailer.{Email, MailerClient}
 
 
 class Mail {
+
+  val subjectMessage = "仮設材ログ削除実施のお知らせ"
+  val level3BodyMessage = "来月１日の深夜０時に仮設材ログと予約情報を削除するバッチ処理を実施します。"
+  val level4BodyMessage = "来月１日の深夜０時に下記の現場の仮設材ログと予約情報を削除するバッチ処理を実施します。"
+
   def sendEmail(mailerClient: MailerClient, user: User, mailInfo: MailInfo)(implicit m: Messages): Unit = {
     val email = new Email
 //    val subjectMsg = Messages("mail.NoticeMail.subject")
-    val subjectMsg = "仮設材ログ削除実施のお知らせ"
+    val subjectMsg = subjectMessage
     email.setSubject(subjectMsg)
     email.setFrom(mailInfo.fromUser)
     if(mailInfo.magType == 1){
@@ -31,7 +36,7 @@ class Mail {
   // 権限３のメッセージ
   def level3Body(mailInfo: MailInfo)(implicit m: Messages): String = {
 //    val bodyMessage = Messages("mail.NoticeMail.Body.level3.message")
-  val bodyMessage = "来月１日の深夜０時に仮設材ログと予約情報を削除するバッチ処理を実施します。"
+  val bodyMessage = level3BodyMessage
 //  Logger.info(s"""${new DateTime().toString("yyyy/MM/dd HH:mm:ss.SSS")}  ${bodyMessage}""")
     bodyMessage
   }
@@ -40,7 +45,7 @@ class Mail {
   def level4Body(mailInfo: MailInfo)(implicit m: Messages): String = {
     val body = new StringBuilder
 //    val bodyMessage = Messages("mail.NoticeMail.Body.level4.message")
-    val bodyMessage = "来月１日の深夜０時に下記の現場の仮設材ログと予約情報を削除するバッチ処理を実施します。"
+    val bodyMessage = level4BodyMessage
     body.append(bodyMessage)
     for(placeName <- mailInfo.placeName){
       body.append("\r\n" + placeName)
