ef7bfc2b4dd5489bb5b1fd5a281cf9efadf10ffd
t-kitahara <t_kitahara@where123.jp>
2018-11-27 16:04:43 +0900

    ・メール送信送信月チェック不具合修正 ・ログ出力箇所の修正

--- app/actors/NoticeMailActor.scala
+++ app/actors/NoticeMailActor.scala
@@ -59,120 +59,123 @@ class NoticeMailActor @Inject()(config: Configuration
     * 実際の処理
     */
   private def execute():Unit = {
-    if ((enableLogging && noticeDaysAgo != 0) &&
-        (noticeDaysAgo >= 4 && noticeDaysAgo <= 20)){
-      var exclusionPlaceID = Array[String]("0")
-      if(!deletionExclusionSite.toString.isEmpty){
-        exclusionPlaceID = deletionExclusionSite.toString().split(",")
-      }
-      var placeNames = List[String]() // 現場名称
-
+    if (enableLogging && noticeDaysAgo != 0) {
+      // noticeDaysAgoの最小値は土日を考えると４日前が良いので環境変数は4日～20日としているが、
+      // 但し平日の場合は最小日を1日でチェックする。
+      //　※ noticeDaysAgo >= MinimumDate
       // メール送信日の曜日を取得
       val deldate = Calendar.getInstance
       var countDay = noticeDaysAgo - 1;
-      deldate.set(Calendar.DATE,(deldate.getActualMaximum(Calendar.DATE) - countDay))
+      deldate.set(Calendar.DATE, (deldate.getActualMaximum(Calendar.DATE) - countDay))
       val week = deldate.get(Calendar.DAY_OF_WEEK)
-      if(week == 1){
+      var MinimumDate = 4
+      if (week == 1) {
         // 日曜日の場合は月曜日にメールを送信
         countDay = countDay - 1
-      }else if(week == 7){
+      } else if (week == 7) {
         // 土曜日の場合は月曜日にメールを送信
         countDay = countDay - 2
+      } else {
+        // 平日の場合は1日前でも可能とする。
+        MinimumDate = 1
       }
+      if (noticeDaysAgo >= MinimumDate && noticeDaysAgo <= 20) {
+        var exclusionPlaceID = Array[String]("0")
+        if (!deletionExclusionSite.toString.isEmpty) {
+          exclusionPlaceID = deletionExclusionSite.toString().split(",")
+        }
+        var placeNames = List[String]() // 現場名称
 
-      val todayDf = new SimpleDateFormat("dd")
-      val df = new SimpleDateFormat("yyyy-MM-")
-      // 当月の最終日を取得
-      val noticeCal = Calendar.getInstance
-      val lastDayOfMonth = noticeCal.getActualMaximum(Calendar.DATE)
-      var today = todayDf.format(noticeCal.getTime()).toInt
-      if(noticeMailTestDate.toString.nonEmpty){
-        // テスト確認用メール送信日が設定されていた場合
-        today = noticeMailTestDate.toString.toInt
-      }
-      // メール送信日か否かをチェック
-      if((lastDayOfMonth - countDay) == today) {
-        if(checkMailAddressRegularity(sendMaileFromUser.toString)) {
-          val cal = Calendar.getInstance
-          val calDel = Calendar.getInstance
-          val delMonth = (deleteInterval - SEND_MAIL_MONTH_DEL) * -1
-          calDel.add(Calendar.MONTH, delMonth); // 先月、先々月分は残す
-          val delateTime = "01 00:00:00" // 1日固定
-          val delateDate = df.format(calDel.getTime()).toString + delateTime
+        val todayDf = new SimpleDateFormat("dd")
+        val df = new SimpleDateFormat("yyyy-MM-")
+        // 当月の最終日を取得
+        val noticeCal = Calendar.getInstance
+        val lastDayOfMonth = noticeCal.getActualMaximum(Calendar.DATE)
+        var today = todayDf.format(noticeCal.getTime()).toInt
+        if (noticeMailTestDate.toString.nonEmpty) {
+          // テスト確認用メール送信日が設定されていた場合
+          today = noticeMailTestDate.toString.toInt
+        }
+        // メール送信日か否かをチェック
+        if ((lastDayOfMonth - countDay) == today) {
+          if (checkMailAddressRegularity(sendMaileFromUser.toString)) {
+            val cal = Calendar.getInstance
+            val calDel = Calendar.getInstance
+            val delMonth = (deleteInterval - SEND_MAIL_MONTH_DEL) * -1
+            calDel.add(Calendar.MONTH, delMonth); // 先月、先々月分は残す
+            val delateTime = "01 00:00:00" // 1日固定
+            val delateDate = df.format(calDel.getTime()).toString + delateTime
 
-          // 削除開始対象月取得
-          // 環境変数（３カ月）を減算
-          val deletionPeriod = (deleteInterval - SEND_MAIL_MONTH) * -1
-          cal.add(Calendar.MONTH, deletionPeriod);
+            // 削除開始対象月取得
+            // 環境変数（３カ月）を減算
+            val deletionPeriod = (deleteInterval - SEND_MAIL_MONTH) * -1
+            cal.add(Calendar.MONTH, deletionPeriod);
 
-          try {
-            // 処理開始
-            Logger.info(s"""${new DateTime().toString("yyyy/MM/dd HH:mm:ss.SSS")}  ${BATCH_NAME} --- start --- """)
+            try {
+              // 処理開始
+              Logger.info(s"""${new DateTime().toString("yyyy/MM/dd HH:mm:ss.SSS")}  ${BATCH_NAME} --- start --- """)
 
-            // DateFormat
-            val ymdf = new SimpleDateFormat("yyyyMM")
-            val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-            // 削除開始対象月
-            val targetMonth = ymdf.format(cal.getTime())
+              // DateFormat
+              val ymdf = new SimpleDateFormat("yyyyMM")
+              val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+              // 削除開始対象月
+              val targetMonth = ymdf.format(cal.getTime())
 
-            val placeData = placeDAO.selectPlaceAll(WORKING_STATUS)
-            placeData.zipWithIndex.foreach { case (place, i) =>
-              // ログ削除除外現場チェック
-              val exclusionId = exclusionPlaceID.find(_ == place.placeId.toString)
-              if (place.btxApiUrl != null && place.btxApiUrl != "" && exclusionId.isEmpty) {
-                val placeId = place.placeId
+              val placeData = placeDAO.selectPlaceAll(WORKING_STATUS)
+              placeData.zipWithIndex.foreach { case (place, i) =>
+                // ログ削除除外現場チェック
+                val exclusionId = exclusionPlaceID.find(_ == place.placeId.toString)
+                if (place.btxApiUrl != null && place.btxApiUrl != "" && exclusionId.isEmpty) {
+                  val placeId = place.placeId
 
-                val itemLogMinData = itemlogDAO.selectOldestRow(placeId)
-                if (itemLogMinData != null && itemLogMinData.length > 0) {
-                  // 最古のレコード　Date型変換
-                  val lastMonth = new DateTime(sdf.parse(itemLogMinData.last.updatetime))
-                  val lastYearMonth = lastMonth.toString("yyyyMM")
-                  // 削除開始対象月が有るか？
-                  if (lastYearMonth.toInt <= targetMonth.toInt) {
-                    // 削除対象データ有り
-                    val itemLogMinData = itemlogDAO.selectOldestRow(placeId, delateDate)
-                    if (itemLogMinData != null && itemLogMinData.length > 0) {
-                      // 仮設材ログ、予約管理削除メール通知
-                      // 現場名称取得
-                      placeNames = placeNames :+ place.placeName
+                  val itemLogMinData = itemlogDAO.selectOldestRow(placeId)
+                  if (itemLogMinData != null && itemLogMinData.length > 0) {
+                    // 最古のレコード　Date型変換
+                    val lastMonth = new DateTime(sdf.parse(itemLogMinData.last.updatetime))
+                    val lastYearMonth = lastMonth.toString("yyyyMM")
+                    // 削除開始対象月が有るか？
+                    if (lastYearMonth.toInt <= targetMonth.toInt) {
+                      // 削除対象データ有り
+                      val itemLogMinData = itemlogDAO.selectOldestRow(placeId, delateDate)
+                      if (itemLogMinData != null && itemLogMinData.length > 0) {
+                        // 仮設材ログ、予約管理削除メール通知
+                        // 現場名称取得
+                        placeNames = placeNames :+ place.placeName
 
-                      var placeName = List[String]() // 現場名称
-                      placeName = placeName :+ place.placeName
-                      val mailer = new Mail
-                      val mailinfo = new MailInfo(MAGTYPE_SITE_MANAGER, sendMaileFromUser.toString, "", placeName)
-                      val users = userDAO.selectSendMailUserList(placeId)
-                      for (user <- users) {
-                        if (checkMailAddressRegularity(user.email.trim)) {
-                          mailer.sendEmail(mailerClient, user, mailinfo)
-                          Logger.info(s"""${new DateTime().toString("yyyy/MM/dd HH:mm:ss.SSS")}  ${BATCH_NAME} --- ${user.name}にメール送信しました。 --- """)
-                        } else {
-                          Logger.error(s"""${user.email}は不正なメールアドレス""")
+                        var placeName = List[String]() // 現場名称
+                        placeName = placeName :+ place.placeName
+                        val mailer = new Mail
+                        val mailinfo = new MailInfo(MAGTYPE_SITE_MANAGER, sendMaileFromUser.toString, "", placeName)
+                        val users = userDAO.selectSendMailUserList(placeId)
+                        for (user <- users) {
+                          if (checkMailAddressRegularity(user.email.trim)) {
+                            mailer.sendEmail(mailerClient, user, mailinfo)
+                          }
                         }
                       }
                     }
                   }
                 }
               }
-            }
 
-            // メールを送信していた場合はdevelopにもメール送信する
-            if (placeNames.nonEmpty) {
-              val mailer = new Mail
-              val users = userDAO.selectSendMailPermission(4)
-              if (developMailAddress != null
-                && developMailAddress.toString.nonEmpty
-                && checkMailAddressRegularity(developMailAddress.toString.trim)) {
-                val mailinfo = new MailInfo(MAGTYPE_DEVELOP, sendMaileFromUser.toString, developMailAddress.toString, placeNames)
-                mailer.sendEmail(mailerClient, users.head, mailinfo)
-                Logger.info(s"""${new DateTime().toString("yyyy/MM/dd HH:mm:ss.SSS")}  ${BATCH_NAME} --- developerにメール送信しました。 --- """)
-                Logger.info(s"""${new DateTime().toString("yyyy/MM/dd HH:mm:ss.SSS")}  ${BATCH_NAME} --- end --- """)
+              // メールを送信していた場合はdevelopにもメール送信する
+              if (placeNames.nonEmpty) {
+                val mailer = new Mail
+                val users = userDAO.selectSendMailPermission(4)
+                if (developMailAddress != null
+                  && developMailAddress.toString.nonEmpty
+                  && checkMailAddressRegularity(developMailAddress.toString.trim)) {
+                  val mailinfo = new MailInfo(MAGTYPE_DEVELOP, sendMaileFromUser.toString, developMailAddress.toString, placeNames)
+                  mailer.sendEmail(mailerClient, users.head, mailinfo)
+                  Logger.info(s"""${new DateTime().toString("yyyy/MM/dd HH:mm:ss.SSS")}  ${BATCH_NAME} --- end --- """)
+                }
               }
+            } catch {
+              case e: Exception =>
+                System.out.println("--------------------バッチエラー検知.start--------------------------")
+                Logger.error(s"""${BATCH_NAME}にてエラーが発生""", e)
+                System.out.println("--------------------バッチエラー検知.end--------------------------")
             }
-          } catch {
-            case e: Exception =>
-              System.out.println("--------------------バッチエラー検知.start--------------------------")
-              Logger.error(s"""${BATCH_NAME}にてエラーが発生""", e)
-              System.out.println("--------------------バッチエラー検知.end--------------------------")
           }
         }
       }
@@ -194,6 +197,7 @@ class NoticeMailActor @Inject()(config: Configuration
     if (matcher.find) {
       true
     }else{
+      Logger.error(s"""${address}は不正なメールアドレス""")
       false
     }
   }
