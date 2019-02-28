d875f95df57a9ae6bb7ed054494727c224f92c9d
p_jung <p_jung@where123.jp>
2019-02-28 12:49:09 +0900

    業者管理変更

--- app/controllers/manage/CompanyController.scala
+++ app/controllers/manage/CompanyController.scala
@@ -23,26 +23,27 @@ import utils.silhouette.MyEnv
 
 // フォームの定義
 case class CompanyUpdateForm(inputPlaceId: String, inputCompanyId: String, inputCompanyName: String, inputNote: String = "")
+
 case class CompanyDeleteForm(deleteCompanyId: String)
 
 @Singleton
-class CompanyManage @Inject()(config: Configuration
-                              , val silhouette: Silhouette[MyEnv]
-                              , val messagesApi: MessagesApi
-                              , companyDAO: models.companyDAO
-                               ) extends BaseController with I18nSupport {
+class CompanyController @Inject()(config: Configuration
+                                  , val silhouette: Silhouette[MyEnv]
+                                  , val messagesApi: MessagesApi
+                                  , companyDAO: models.companyDAO
+                                 ) extends BaseController with I18nSupport {
 
 
   /** 初期表示 */
   def index = SecuredAction { implicit request =>
     val reqIdentity = request.identity
-    if(reqIdentity.level >= 2){
+    if (reqIdentity.level >= 2) {
       // 選択された現場の現場ID
       val placeId = super.getCurrentPlaceId
       // 業者情報
       val companyList = companyDAO.selectCompany(placeId)
-      Ok(views.html.manage.companyManage(companyList, placeId))
-    }else {
+      Ok(views.html.manage.company(companyList, placeId))
+    } else {
       Redirect(site.routes.ItemCarMaster.index)
     }
   }
@@ -56,59 +57,61 @@ class CompanyManage @Inject()(config: Configuration
     val inputForm = Form(mapping(
       "inputPlaceId" -> text
       , "inputCompanyId" -> text
-      , "inputCompanyName" -> text.verifying(Messages("error.cms.CompanyManage.update.inputCompanyName.empty"), {!_.isEmpty})
+      , "inputCompanyName" -> text.verifying(Messages("error.cms.CompanyManage.update.inputCompanyName.empty"), {
+        !_.isEmpty
+      })
       , "inputNote" -> text
     )(CompanyUpdateForm.apply)(CompanyUpdateForm.unapply))
 
     // フォームの取得
     val form = inputForm.bindFromRequest
-    if (form.hasErrors){
+    if (form.hasErrors) {
       // エラーメッセージ
       val errMsg = form.errors.map(_.message).mkString(HTML_BR)
       // リダイレクトで画面遷移
-      Redirect(routes.CompanyManage.index()).flashing(ERROR_MSG_KEY -> errMsg)
-    }else{
+      Redirect(routes.CompanyController.index()).flashing(ERROR_MSG_KEY -> errMsg)
+    } else {
       var errMsg = Seq[String]()
       val f = form.get
-      if(f.inputCompanyId.isEmpty){
+      if (f.inputCompanyId.isEmpty) {
         // 新規登録の場合 --------------------------
         // 名称重複チェック
         val companyList = companyDAO.selectCompany(super.getCurrentPlaceId, f.inputCompanyName)
-        if(companyList.nonEmpty){
+        if (companyList.nonEmpty) {
           errMsg :+= Messages("error.cms.CompanyManage.update.inputCompanyName.duplicate", f.inputCompanyName)
         }
-        if(errMsg.nonEmpty){
+        if (errMsg.nonEmpty) {
           // エラーで遷移
-          Redirect(routes.CompanyManage.index)
+          Redirect(routes.CompanyController.index)
             .flashing(ERROR_MSG_KEY -> errMsg.mkString(HTML_BR))
-        }else{
+        } else {
           // DB処理
           companyDAO.insert(f.inputCompanyName, f.inputNote, f.inputPlaceId.toInt)
 
-          Redirect(routes.CompanyManage.index)
+          Redirect(routes.CompanyController.index)
             .flashing(SUCCESS_MSG_KEY -> Messages("success.cms.CompanyManage.update"))
         }
 
-      }else{
+      } else {
         // 更新の場合 --------------------------
         // 名称重複チェック
         var companyList = companyDAO.selectCompany(super.getCurrentPlaceId)
         companyList = companyList.filter(_.companyId != f.inputCompanyId.toInt).filter(_.companyName == f.inputCompanyName)
-        if(companyList.nonEmpty){
+        if (companyList.nonEmpty) {
           errMsg :+= Messages("error.cms.CompanyManage.update.inputCompanyName.duplicate", f.inputCompanyName)
         }
 
         // 予約情報テーブルに作業車・立馬IDが存在していないか
         val itemReserveList = companyDAO.selectCarReserveCheck(super.getCurrentPlaceId, f.inputCompanyId.toInt)
-        if(itemReserveList.length > 0){
-          var chkFlg : Int = 0
-          var companyNm : String = ""
+        if (itemReserveList.length > 0) {
+          var chkFlg: Int = 0
+          var companyNm: String = ""
           // 現在時刻設定
           val mSimpleDateFormat = new SimpleDateFormat("yyyyMMdd", Locale.JAPAN)
           val currentTime = new Date();
           val mTime = mSimpleDateFormat.format(currentTime)
           val mHour = new SimpleDateFormat("HH").format(Calendar.getInstance().getTime()).toInt
-          for(itemReserve <- itemReserveList) {
+          for (itemReserve <- itemReserveList) {
             companyNm = itemReserve.companyName
             if (itemReserve.reserveEndDate.toInt < mTime.toInt) {
               // 予約期間が前日の場合、または期間が終日の場合
@@ -130,7 +133,7 @@ class CompanyManage @Inject()(config: Configuration
                       chkFlg = 2
                     }
                   }
-                }else{
+                } else {
                   // 予約済み（使用中）
                   chkFlg = 1
                 }
@@ -146,7 +149,7 @@ class CompanyManage @Inject()(config: Configuration
                       chkFlg = 2
                     }
                   }
-                } else{
+                } else {
                   // 予約済み（使用中）
                   chkFlg = 1
                 }
@@ -156,25 +159,25 @@ class CompanyManage @Inject()(config: Configuration
               }
             }
           }
-          if(chkFlg == 1){
+          if (chkFlg == 1) {
             errMsg :+= Messages("error.cms.CompanyManage.update.inputCompanyName.use.noChange", companyNm);
-          }else if(chkFlg == 2){
-            if(reqIdentity.level < 3) {
+          } else if (chkFlg == 2) {
+            if (reqIdentity.level < 3) {
               // 権限がレベル３以下のみ予約情報が有る場合エラーにする
               errMsg :+= Messages("error.cms.CompanyManage.update.inputCompanyName.use.exceed", companyNm);
             }
           }
         }
 
-        if(errMsg.nonEmpty){
+        if (errMsg.nonEmpty) {
           // エラーで遷移
-          Redirect(routes.CompanyManage.index)
+          Redirect(routes.CompanyController.index)
             .flashing(ERROR_MSG_KEY -> errMsg.mkString(HTML_BR))
-        }else{
+        } else {
           // DB処理
           companyDAO.updateById(f.inputCompanyId.toInt, f.inputCompanyName, f.inputNote)
 
-          Redirect(routes.CompanyManage.index)
+          Redirect(routes.CompanyController.index)
             .flashing(SUCCESS_MSG_KEY -> Messages("success.cms.CompanyManage.update"))
         }
       }
@@ -189,7 +192,9 @@ class CompanyManage @Inject()(config: Configuration
     var errMsg = Seq[String]()
     // フォームの準備
     val inputForm = Form(mapping(
-      "deleteCompanyId" -> text.verifying(Messages("error.manage.ItemCar.delete.empty"), {!_.isEmpty})
+      "deleteCompanyId" -> text.verifying(Messages("error.manage.ItemCar.delete.empty"), {
+        !_.isEmpty
+      })
     )(CompanyDeleteForm.apply)(CompanyDeleteForm.unapply))
 
     // フォームの取得
@@ -198,20 +203,20 @@ class CompanyManage @Inject()(config: Configuration
       // エラーメッセージ
       val errMsg = form.errors.map(_.message).mkString(HTML_BR)
       // リダイレクトで画面遷移
-      Redirect(routes.CompanyManage.index).flashing(ERROR_MSG_KEY -> errMsg)
+      Redirect(routes.CompanyController.index).flashing(ERROR_MSG_KEY -> errMsg)
     } else {
       val f = form.get
       // 予約情報テーブルに作業車・立馬IDが存在していないか
       val itemReserveList = companyDAO.selectCarReserveCheck(super.getCurrentPlaceId, f.deleteCompanyId.toInt)
-      if(itemReserveList.length > 0){
-        var chkFlg : Int = 0
-        var companyNm : String = ""
+      if (itemReserveList.length > 0) {
+        var chkFlg: Int = 0
+        var companyNm: String = ""
         // 現在時刻設定
         val mSimpleDateFormat = new SimpleDateFormat("yyyyMMdd", Locale.JAPAN)
         val currentTime = new Date();
         val mTime = mSimpleDateFormat.format(currentTime)
         val mHour = new SimpleDateFormat("HH").format(Calendar.getInstance().getTime()).toInt
-        for(itemReserve <- itemReserveList) {
+        for (itemReserve <- itemReserveList) {
           companyNm = itemReserve.companyName
           if (itemReserve.reserveEndDate.toInt < mTime.toInt) {
             // 予約期間が前日の場合、または期間が終日の場合
@@ -233,7 +238,7 @@ class CompanyManage @Inject()(config: Configuration
                     chkFlg = 2
                   }
                 }
-              }else{
+              } else {
                 // 予約済み（使用中）
                 chkFlg = 1
               }
@@ -249,7 +254,7 @@ class CompanyManage @Inject()(config: Configuration
                     chkFlg = 2
                   }
                 }
-              } else{
+              } else {
                 // 予約済み（使用中）
                 chkFlg = 1
               }
@@ -259,25 +264,25 @@ class CompanyManage @Inject()(config: Configuration
             }
           }
         }
-        if(chkFlg == 1){
+        if (chkFlg == 1) {
           errMsg :+= Messages("error.cms.CompanyManage.delete.inputCompanyName.use.noChange", companyNm);
-        }else if(chkFlg == 2){
-          if(reqIdentity.level < 3) {
+        } else if (chkFlg == 2) {
+          if (reqIdentity.level < 3) {
             // 権限がレベル３以下のみ予約情報が有る場合エラーにする
             errMsg :+= Messages("error.cms.CompanyManage.delete.inputCompanyName.use.exceed", companyNm);
           }
         }
       }
-      if(errMsg.nonEmpty){
+      if (errMsg.nonEmpty) {
         // エラーで遷移
-        Redirect(routes.CompanyManage.index)
+        Redirect(routes.CompanyController.index)
           .flashing(ERROR_MSG_KEY -> errMsg.mkString(HTML_BR))
-      }else {
+      } else {
         // DB処理
         companyDAO.deleteById(f.deleteCompanyId.toInt)
 
         // リダイレクト
-        Redirect(routes.CompanyManage.index).flashing(SUCCESS_MSG_KEY -> Messages("success.cms.CompanyManage.delete"))
+        Redirect(routes.CompanyController.index).flashing(SUCCESS_MSG_KEY -> Messages("success.cms.CompanyManage.delete"))
       }
     }
   }
